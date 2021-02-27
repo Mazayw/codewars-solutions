b1ae1a4280564f7897d95721a25005ae512f346e
Mazayw <mazayw@gmail.com>
2021-02-27 04:48:00 +0000

    feat: add solution Array.diff

---
+++ 6 kyu/arraydiff.js
@@ -0,0 +1,4 @@
+function arrayDiff(a, b) {
+return a.filter(number => b.indexOf(number) === -1);
+}
+
---
+++ 6 kyu/arraydiff_2.js
@@ -0,0 +1,9 @@
+function arrayDiff(a, b) {
+  let c=[];
+for (let i=0; i<a.length; i++){
+ if (b.indexOf(a[i],0)==-1) {
+ c.push(a[i]);
+ }
+}
+return c
+}
