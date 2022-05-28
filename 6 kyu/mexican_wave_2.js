9ce375503ff972ba2afa2369dbd3917909f4ed7b
Mazayw <mazayw@gmail.com>
2022-05-29 02:38:38 +0300

    feat: add solution Mexican Wave

---
+++ 6 kyu/mexican_wave_2.js
@@ -0,0 +1,11 @@
+function wave(str) {
+  const result = [];
+  for (let i = 0; i < str.length; i++) {
+    const word = str.split('');
+    if (!!word[i].match(/[\d,\w,]/g)) {
+      word[i] = word[i].toUpperCase();
+      result.push(word.join(''));
+    }
+  }
+  return result;
+}
